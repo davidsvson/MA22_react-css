aa9acdeb43c348648c7cfd59964b10b025e7971c
David Svensson <davidsvson@gmail.com>
2023-09-27 11:25:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import cat1 from './assets/img/cat1.jpeg'
+import './App.css'
+import Gallery from './components/Gallery'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
